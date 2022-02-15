eb27cbd995b44cf4099ced8dc1bf06c601113b84
irf-dev <irfan.4444.maulana@gmail.com>
2022-02-15 20:58:11 +0700

    tugas 2 javascript
    penyerahan tugas

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var kosong;
+nomor = 40;
+huruf = "lulus atau tidaknya";
+lulus = 70;
+kosong2 = null;
+angka = [1, 4, 7, 9, 12];
+
+if (lulus > 75)
+    lulus = true;
+else
+    lulus = false;
+
+console.log(nomor, huruf, lulus, kosong, kosong2, angka);
